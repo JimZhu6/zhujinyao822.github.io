7491ca4d96d50c7549a41cff834f64a0f804519c
zhujinyao <425669746@qq.com>
2018-11-27 17:19:47 +0800

    edit gulp updata gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-var gulp = require('gulp');
-
-    //Plugins模块获取
-    var minifycss = require('gulp-minify-css');
-    var uglify = require('gulp-uglify');
-    var htmlmin = require('gulp-htmlmin');
-    var htmlclean = require('gulp-htmlclean');
-
-    // 压缩 public 目录 css文件
-    gulp.task('minify-css', function() {
-        return gulp.src('./public/**/*.css')
-            .pipe(minifycss())
-            .pipe(gulp.dest('./public'));
-    });
-
-    // 压缩 public 目录 html文件
-    gulp.task('minify-html', function() {
-      return gulp.src('./public/**/*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-             removeComments: true,
-             minifyJS: true,
-             minifyCSS: true,
-             minifyURLs: true,
-        }))
-        .pipe(gulp.dest('./public'))
-    });
-
-    // 压缩 public/js 目录 js文件
-    gulp.task('minify-js', function() {
-        return gulp.src('./public/**/*.js')
-            .pipe(uglify())
-            .pipe(gulp.dest('./public'));
-    });
-
-
-    // 执行 gulp 命令时执行的任务
-    gulp.task('default', [
-        'minify-html','minify-css','minify-js'
-    ]);
+﻿var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imageMin = require('gulp-imagemin');
+// 压缩 public 目录 css
+gulp.task('minify-css', function () {
+  return gulp.src('./public/**/*.css')
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public'));
+});
+// 压缩 public 目录 html
+gulp.task('minify-html', function () {
+  return gulp.src('./public/**/*.html')
+    .pipe(htmlclean())
+    .pipe(htmlmin({
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    }))
+    .pipe(gulp.dest('./public'))
+});
+// 压缩 public/js 目录 js
+gulp.task('minify-js', function () {
+  return gulp.src('./public/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'));
+});
+// 压缩图片
+gulp.task('images', function () {
+  return gulp.src('./public/**/*.{png,jpg,gif,ico,svg}')
+    .pipe(imageMin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('./public'))
+})
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series('minify-css', 'minify-html', 'minify-js', 'images'));
